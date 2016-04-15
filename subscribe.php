60d7f58f057efaf5f4cefb3eaeeedb780ab226f8
Srinivas Maroju <marojupavan@gmail.com>
2016-04-15 02:07:51 +0000

    Working auth

--- subscribe.php
+++ subscribe.php
@@ -8,26 +8,39 @@ const CLIENT_SECRET = '0a856487889789c6f6ca1c691fdd9cc3';
 
 const REDIRECT_URI           = 'https://www.pavans-world.com/fitbit/subscribe.php';
 const AUTHORIZATION_ENDPOINT = 'https://www.fitbit.com/oauth2/authorize';
-const TOKEN_ENDPOINT         = 'https://api.fitbit.com/oauth2/access_token';
+const TOKEN_ENDPOINT         = 'https://api.fitbit.com/oauth2/token';
 
-$client = new OAuth2\Client(CLIENT_ID, CLIENT_SECRET);
+$client = new OAuth2\Client(CLIENT_ID, CLIENT_SECRET, OAuth2\Client::AUTH_TYPE_AUTHORIZATION_BASIC);
+
 if (!isset($_GET['code']))
 {
     //Set the scope
-    $scope = ["activity","heartrate","location","nutrition","profile","sleep"];
-    $scope_space_delimited_array = implode("", $scope);
+    $scope = array("activity","heartrate","location","nutrition","profile","sleep");
     
-    $auth_url = $client->getAuthenticationUrl(AUTHORIZATION_ENDPOINT, REDIRECT_URI , array("scope" => $scope_space_delimited_array));
-    print  $auth_url; exit;
+    $scope_space_delimited_array = implode(" ", $scope);
+    $extra_args = array("scope" => $scope_space_delimited_array);
+  
+    $auth_decoded = $client->getAuthenticationUrl(AUTHORIZATION_ENDPOINT, REDIRECT_URI , $extra_args);
+    $auth_url = str_replace("+","%20", $auth_decoded);
+    //print  $auth_url; exit;
     header('Location: ' . $auth_url);
     die('Redirect');
 }
 else
 {
+    #echo "Test"; exit;
     $params = array('code' => $_GET['code'], 'redirect_uri' => REDIRECT_URI);
+    //echo "geting token"; exit;
     $response = $client->getAccessToken(TOKEN_ENDPOINT, 'authorization_code', $params);
+    
     parse_str($response['result'], $info);
     $client->setAccessToken($info['access_token']);
-    $response = $client->fetch('https://graph.facebook.com/me');
-    var_dump($response, $response['result']);
+    //$response = $client->fetch('https://graph.facebook.com/me');
+    if($response['code'] = 200) {
+	var_dump($response, $response['result']);
+    } else {
+	print "some error occured";
+	print_r($response); exit;	
+    }
+
 }
