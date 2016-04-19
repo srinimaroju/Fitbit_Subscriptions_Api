499ccbb6b9f9c8926e92fe9c44f1605ef58476a9
Srinivas Maroju <srinivas.maroju@nbcuni.com>
2016-04-18 23:14:07 -0400

    Home Page for the project

--- app/app.component.js
+++ app/app.component.js
@@ -2,7 +2,7 @@
   app.AppComponent =
     ng.core.Component({
       selector: '.cover-heading',
-      template: '<h1>My First Angular 2 App</h1>'
+      template: '<h1>Start your morning with a smile</h1>'
     })
     .Class({
       constructor: function() {}
